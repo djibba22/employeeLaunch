3148a264859dfdf775ee1003e1caf77303437203
djibba22 <johnwaynedinsmore@gmail.com>
2020-08-10 14:46:52 -0400

    first commit

---
+++ build/precache-manifest.5fd9080cea459f50e590389f8cc17e25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d73530154337ed1a4e948b83a8590c56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae4292fd98b18d427a23",
+    "url": "/static/css/main.16f2466d.chunk.css"
+  },
+  {
+    "revision": "98d381b8539990c9da13",
+    "url": "/static/js/2.88f9d077.chunk.js"
+  },
+  {
+    "revision": "ae4292fd98b18d427a23",
+    "url": "/static/js/main.eab77a96.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
