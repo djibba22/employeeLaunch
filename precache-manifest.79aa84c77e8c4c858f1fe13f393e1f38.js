4b5b61c7bcbc9a93d22a5d2a633c01ee058d728d
djibba22 <johnwaynedinsmore@gmail.com>
2020-08-10 14:50:46 -0400

    Updates

---
+++ precache-manifest.79aa84c77e8c4c858f1fe13f393e1f38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51b2841f377ffe61049dd29a165cf01e",
+    "url": "/employeeLaunch/index.html"
+  },
+  {
+    "revision": "1dacca4864334f9f99da",
+    "url": "/employeeLaunch/static/css/main.16f2466d.chunk.css"
+  },
+  {
+    "revision": "98d381b8539990c9da13",
+    "url": "/employeeLaunch/static/js/2.88f9d077.chunk.js"
+  },
+  {
+    "revision": "1dacca4864334f9f99da",
+    "url": "/employeeLaunch/static/js/main.e186bbd2.chunk.js"
+  },
+  {
+    "revision": "0b862ce40064fa67fd43",
+    "url": "/employeeLaunch/static/js/runtime~main.7bc1f553.js"
+  }
+]);
